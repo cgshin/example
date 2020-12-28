399db32f8dabd4a1d7dcf7c55a8b4132a1ae7d73
cgshin <cgshin74@gmail.com>
2020-12-28 22:09:13 +0900

    ExampleController

---
+++ src/main/java/com/test/ExampleController.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+	public Map<String, String> init(){
+		Map<String, String> map = new HashMap<>();
+		map.put("id", "test1");
+		map.put("name", "ев╫╨ем1");
+		map.put("message", "Hello~~");
+		
+		return map;
+	}
+}
